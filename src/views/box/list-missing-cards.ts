99bed499eed1ca73fb18b9dc4de03aa73e8385bc
Kevin Rutherford <kevin@rutherford-software.com>
2024-02-01 13:23:55 +0000

    Provide dummy Rookery data from the /collections endpoint

--- src/views/box/list-missing-cards.ts
+++ src/views/box/list-missing-cards.ts
@@ -6,6 +6,22 @@ export const getCollections = (): View => () => pipe(
   {
     type: 'Collections',
     data: [
+      {
+        name: 'CHS',
+        description: 'Papers under review by the CHS project',
+        papersCount: 12,
+        commentsCount: 23,
+        followersCount: 4,
+        lastActivityAt: '4 hours ago',
+      },
+      {
+        name: 'PRU3',
+        description: 'Papers to be referenced by the PRU3 project',
+        papersCount: 134,
+        commentsCount: 258,
+        followersCount: 11,
+        lastActivityAt: '3 days ago',
+      },
     ],
   },
   E.right,
