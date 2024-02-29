f4bd57f4c8bed9f353e4fe3c259c26d814f4df46
Kevin Rutherford <kevin@rutherford-software.com>
2024-02-29 11:46:47 +0000

    Move hardcoded collections into a nascent readmodel query #30

--- src/views/get-collections.ts
+++ src/views/get-collections.ts
@@ -2,39 +2,7 @@ import * as E from 'fp-ts/Either'
 import * as RA from 'fp-ts/ReadonlyArray'
 import { pipe } from 'fp-ts/function'
 import { View } from './view'
-
-type Collection = {
-  id: string,
-  name: string,
-  description: string,
-  papersCount: number,
-  commentsCount: number,
-  followersCount: number,
-  lastActivityAt: Date,
-}
-
-const allCollections = (): ReadonlyArray<Collection> => {
-  return [
-    {
-      id: 'chs',
-      name: 'CHS',
-      description: 'Papers under review by the CHS project',
-      papersCount: 12,
-      commentsCount: 23,
-      followersCount: 4,
-      lastActivityAt: new Date(),
-    },
-    {
-      id: 'pru3',
-      name: 'PRU3',
-      description: 'Papers to be referenced by the PRU3 project',
-      papersCount: 134,
-      commentsCount: 258,
-      followersCount: 11,
-      lastActivityAt: new Date(),
-    },
-  ]
-}
+import { allCollections } from '../readmodels/all-collections'
 
 export const getCollections = (): View => () => pipe(
   allCollections(),
