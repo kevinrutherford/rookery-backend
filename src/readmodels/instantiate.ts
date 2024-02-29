582f5d1ee6bc36f9fe1749e3923e3d4bf159cb03
Kevin Rutherford <kevin@rutherford-software.com>
2024-02-29 14:07:29 +0000

    Subscribe to events from the eventstore #30

--- src/readmodels/instantiate.ts
+++ src/readmodels/instantiate.ts
@@ -1,3 +1,4 @@
+import { EventStoreDBClient, excludeSystemEvents, START } from '@eventstore/db-client'
 import { allCollections, Collection } from './all-collections'
 import { Queries } from './queries'
 
@@ -22,6 +23,17 @@ export const instantiate = (): Queries => {
       lastActivityAt: new Date(),
     }],
   ])
+  const client = EventStoreDBClient.connectionString('esdb://eventstore:2113?tls=false&keepAliveTimeout=10000&keepAliveInterval=10000')
+  const subscription = client.subscribeToAll({
+    fromPosition: START,
+    filter: excludeSystemEvents(),
+  })
+
+  subscription.on('data', (event) => {
+    console.log(event)
+    console.log(`Received event ${event.event?.revision}@${event.event?.streamId}`)
+  })
+
   return ({
     allCollections: allCollections(currentState),
   })
