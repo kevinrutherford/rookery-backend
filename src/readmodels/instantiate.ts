167d0e4004f0e07fa3fb6076539364be24ac46ed
Kevin Rutherford <kevin@rutherford-software.com>
2024-02-29 16:14:22 +0000

    Replace hardcoded events by an event handler #30

--- src/readmodels/instantiate.ts
+++ src/readmodels/instantiate.ts
@@ -1,37 +1,40 @@
-import { EventStoreDBClient, excludeSystemEvents, START } from '@eventstore/db-client'
+import { EventStoreDBClient, excludeSystemEvents, JSONEventType, RecordedEvent, START } from '@eventstore/db-client'
 import { allCollections, Collection } from './all-collections'
 import { Queries } from './queries'
 
+type CollectionCreatedEvent = JSONEventType<'collection-created', {
+  id: string,
+  handle: string,
+  name: string,
+  description: string,
+}>
+
+type HandledEvents = CollectionCreatedEvent
+
 export const instantiate = (): Queries => {
-  const currentState: Map<string, Collection> = new Map([
-    ['chs', {
-      id: 'chs',
-      name: 'CHS',
-      description: 'Papers under review by the CHS project',
-      papersCount: 12,
-      commentsCount: 23,
-      followersCount: 4,
-      lastActivityAt: new Date(),
-    }],
-    ['pru3', {
-      id: 'pru3',
-      name: 'PRU3',
-      description: 'Papers to be referenced by the PRU3 project',
-      papersCount: 134,
-      commentsCount: 258,
-      followersCount: 11,
-      lastActivityAt: new Date(),
-    }],
-  ])
+  const currentState: Map<string, Collection> = new Map()
   const client = EventStoreDBClient.connectionString('esdb://eventstore:2113?tls=false&keepAliveTimeout=10000&keepAliveInterval=10000')
   const subscription = client.subscribeToAll({
     fromPosition: START,
     filter: excludeSystemEvents(),
   })
 
-  subscription.on('data', (event) => {
+  subscription.on('data', (resolvedEvent) => {
+    const event = resolvedEvent.event
+    if (!event)
+      return
     console.log(event)
-    console.log(`Received event ${event.event?.revision}@${event.event?.streamId}`)
+    console.log(`Received event ${event.revision}@${event.streamId}`)
+    const x = event as RecordedEvent<HandledEvents>
+    if (x.type === 'collection-created') {
+      currentState.set(x.data.id, {
+        ...x.data,
+        papersCount: 0,
+        commentsCount: 0,
+        followersCount: 0,
+        lastActivityAt: new Date(),
+      })
+    }
   })
 
   return ({
