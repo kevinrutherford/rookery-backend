43c6e7d69654ef19e80f27b0daeee2da7a07e690
Kevin Rutherford <kevin@rutherford-software.com>
2024-02-29 13:37:03 +0000

    Separate the readmodel query from the current state of the readmodel #30

--- src/readmodels/all-collections.ts
+++ src/readmodels/all-collections.ts
@@ -8,26 +8,7 @@ export type Collection = {
   lastActivityAt: Date,
 }
 
-export const allCollections = (): ReadonlyArray<Collection> => {
-  return [
-    {
-      id: 'chs',
-      name: 'CHS',
-      description: 'Papers under review by the CHS project',
-      papersCount: 12,
-      commentsCount: 23,
-      followersCount: 4,
-      lastActivityAt: new Date(),
-    },
-    {
-      id: 'pru3',
-      name: 'PRU3',
-      description: 'Papers to be referenced by the PRU3 project',
-      papersCount: 134,
-      commentsCount: 258,
-      followersCount: 11,
-      lastActivityAt: new Date(),
-    },
-  ]
+export const allCollections = (currentState: ReadonlyArray<Collection>) => (): ReadonlyArray<Collection> => {
+  return currentState
 }
 
--- src/readmodels/instantiate.ts
+++ src/readmodels/instantiate.ts
@@ -1,6 +1,28 @@
-import { allCollections } from './all-collections'
+import { allCollections, Collection } from './all-collections'
 import { Queries } from './queries'
 
-export const instantiate = (): Queries => ({
-  allCollections,
-})
+export const instantiate = (): Queries => {
+  const currentState: ReadonlyArray<Collection> = [
+    {
+      id: 'chs',
+      name: 'CHS',
+      description: 'Papers under review by the CHS project',
+      papersCount: 12,
+      commentsCount: 23,
+      followersCount: 4,
+      lastActivityAt: new Date(),
+    },
+    {
+      id: 'pru3',
+      name: 'PRU3',
+      description: 'Papers to be referenced by the PRU3 project',
+      papersCount: 134,
+      commentsCount: 258,
+      followersCount: 11,
+      lastActivityAt: new Date(),
+    },
+  ]
+  return ({
+    allCollections: allCollections(currentState),
+  })
+}
