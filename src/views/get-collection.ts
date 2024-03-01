678d9caf8669869b0d1f6132ab460bf074cd0932
Kevin Rutherford <kevin@rutherford-software.com>
2024-03-01 11:23:41 +0000

    Prepare to send optional frontMatter in entries #33

--- src/views/get-collection.ts
+++ src/views/get-collection.ts
@@ -23,22 +23,28 @@ export const getCollection = (): View => (input) => pipe(
       entries: [
         {
           id: '72fe90a8-38db-4635-81db-1e78501fce22',
-          title: 'A General Framework for Analyzing Sustainability of Social-Ecological Systems',
           doi: '10.1126/science.1172133',
+          frontMatter: {
+            title: 'A General Framework for Analyzing Sustainability of Social-Ecological Systems',
+          },
           commentsCount: 7,
           latestActivityAt: 'yesterday',
         },
         {
           id: '97ab7e35-5baa-4ddd-bd2f-eb41d1edf9b8',
-          title: 'Implementing the Additional Roles Reimbursement Scheme in 7 English PCNs: a qualitative study',
           doi: '10.3399/BJGP.2023.0216',
+          frontMatter: {
+            title: 'Implementing the Additional Roles Reimbursement Scheme in 7 English PCNs: a qualitative study',
+          },
           commentsCount: 19,
           latestActivityAt: '4 hours ago',
         },
         {
           id: '036b2df7-a67c-4a40-9bad-0438978f8e07',
-          title: 'INTERROGATING INSTITUTIONAL CHANGE: ACTORS\' ATTITUDES TO COMPETITION AND COOPERATION IN COMMISSIONING HEALTH SERVICES IN ENGLAND',
           doi: '10.1111/padm.12268',
+          frontMatter: {
+            title: 'INTERROGATING INSTITUTIONAL CHANGE: ACTORS\' ATTITUDES TO COMPETITION AND COOPERATION IN COMMISSIONING HEALTH SERVICES IN ENGLAND',
+          },
           commentsCount: 19,
           latestActivityAt: '2 days ago',
         },
