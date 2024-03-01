a2442ed4e6507ffabe267ad0ed1dece64bfd3f6b
Kevin Rutherford <kevin@rutherford-software.com>
2024-03-01 17:16:37 +0000

    Define the type of a route handler #33

--- src/http/ping.ts
+++ src/http/ping.ts
@@ -1,10 +1,7 @@
-import { NextFunction, Request, Response } from 'express'
+import { Request, Response } from 'express'
 
-export default () => (
-  async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    res.set('Cache-Control', 'no-store, must-revalidate')
-    res.status(200).send('pong')
-    next()
-  }
-)
+export const ping = (req: Request, res: Response): void => {
+  res.set('Cache-Control', 'no-store, must-revalidate')
+  res.status(200).send('pong')
+}
 
--- src/http/router.ts
+++ src/http/router.ts
@@ -1,18 +1,28 @@
-import { Router } from 'express'
+import { Request, Response, Router } from 'express'
 import { executeView } from './execute-view'
 import { Logger } from './logger'
-import ping from './ping'
+import { ping } from './ping'
 import { Views } from '../views'
+
+type RouteHandler = (req: Request, res: Response) => void
+
+type RouteHandlers = {
+  about: RouteHandler,
+  collections: RouteHandler,
+  collection: RouteHandler,
+  entry: RouteHandler,
+  ping: RouteHandler,
+}
 
 export const router = (views: Views, logger: Logger): Router => {
   const r = Router()
 
-  const routeHandlers = {
+  const routeHandlers: RouteHandlers = {
     about: executeView(logger)(views.getAbout),
     collections: executeView(logger)(views.getCollections),
     collection: executeView(logger)(views.getCollection),
     entry: executeView(logger)(views.getEntry),
-    ping: ping(),
+    ping,
   }
 
   r.get('/ping', routeHandlers.ping)
