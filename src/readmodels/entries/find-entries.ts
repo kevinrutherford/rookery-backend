39fc6d03f8d48ded4e817898758cc4bf6cea8d26
Kevin Rutherford <kevin@rutherford-software.com>
2024-03-03 14:03:52 +0000

    Implement the query for entries #33

--- src/readmodels/entries/find-entries.ts
+++ src/readmodels/entries/find-entries.ts
@@ -1,7 +1,16 @@
+import * as O from 'fp-ts/Option'
+import * as RM from 'fp-ts/ReadonlyMap'
+import { identity, pipe } from 'fp-ts/function'
+import * as S from 'fp-ts/string'
 import { Entry } from './entry'
 import { Readmodel } from './readmodel'
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const findEntries = (currentState: Readmodel) => (collectionId: string): ReadonlyArray<Entry> => [
-]
+export const findEntries = (currentState: Readmodel) => (collectionId: string): ReadonlyArray<Entry> => pipe(
+  currentState,
+  RM.lookup(S.Eq)(collectionId),
+  O.match(
+    () => [],
+    identity,
+  ),
+)
 
