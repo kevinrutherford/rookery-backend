12d2821d8b0fa357200d5d14070975be6bfd37e8
Kevin Rutherford <kevin@rutherford-software.com>
2024-03-04 15:36:40 +0000

    Remove hardcoded comments in favour of those found in the database #33

--- src/views/get-entry.ts
+++ src/views/get-entry.ts
@@ -22,30 +22,7 @@ export const getEntry = (queries: Queries): View => (input) => pipe(
     type: 'Entry',
     data: {
       ...entry,
-      comments: [
-        {
-          id: 'ea36343f-97b2-4908-86a5-1f5a4f623297',
-          author: '@DonnaB',
-          content: 'This looks great',
-          timestamp: '2024-02-18T19:08:55',
-          replies: [
-            {
-              id: 'e31a2757-0d7c-4e51-965b-28b079a1fd7a',
-              author: '@khcheck',
-              content: 'I agree',
-              timestamp: '2024-02-18T19:08:55',
-              replies: [],
-            },
-          ],
-        },
-        {
-          id: '89a82f65-4752-4db0-a271-effe3336dd68',
-          author: '@mgoff',
-          content: 'Good for me',
-          timestamp: '2024-02-18T19:08:55',
-          replies: [],
-        },
-      ],
+      comments: queries.findComments(entry.id),
     },
   })),
 )
