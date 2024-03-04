d9e736a25a2a4c4d4e7ad59cb651cd214e3ccfb7
Kevin Rutherford <kevin@rutherford-software.com>
2024-03-04 09:49:14 +0000

    Remove an outdated eslint override

--- src/readmodels/entries/handle-event.ts
+++ src/readmodels/entries/handle-event.ts
@@ -2,7 +2,6 @@ import { RecordedEvent } from '@eventstore/db-client'
 import { Readmodel } from './readmodel'
 import { DomainEvent } from '../domain-event'
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const handleEvent = (state: Readmodel) => (event: RecordedEvent<DomainEvent>): void => {
   if (event.type === 'doi-entered') {
     const data = event.data
