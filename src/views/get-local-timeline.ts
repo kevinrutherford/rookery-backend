0f0e43eac7f13d5369b1ae7f3cb66790dbf9376a
Kevin Rutherford <kevin@rutherford-software.com>
2024-03-15 11:24:34 +0000

    Add a hardcoded route returning the local timeline #41

---
+++ src/views/get-local-timeline.ts
@@ -0,0 +1,55 @@
+import * as E from 'fp-ts/Either'
+import { pipe } from 'fp-ts/function'
+import { View } from '../http/index.open'
+
+export const getLocalTimeline = (): View => () => pipe(
+  {
+    type: 'Timeline',
+    data: [
+      {
+        userHandle: 'mgoff',
+        timestamp: '1h',
+        action: 'commented on a paper in collection CHS',
+        content: 'Totally',
+      },
+      {
+        userHandle: 'khcheck',
+        timestamp: '1h',
+        action: 'commented on a paper in collection CHS',
+        content: 'This is just awesome, dude!',
+      },
+      {
+        userHandle: 'DonnaB',
+        timestamp: '3h',
+        action: 'added a paper to collection CHS',
+        content: '10.1111/padm.12268',
+      },
+      {
+        userHandle: 'DonnaB',
+        timestamp: '3h',
+        action: 'added a paper to collection CHS',
+        content: '10.1126/science.1172133',
+      },
+      {
+        userHandle: 'khcheck',
+        timestamp: '4h',
+        action: 'added a paper to collection CHS',
+        content: '10.3399/BJGP.2023.0216',
+      },
+      {
+        userHandle: 'mgoff',
+        timestamp: '1d',
+        action: 'created collection PRU3',
+        content: 'Project PRU3 review inputs.',
+      },
+      {
+        userHandle: 'DonnaB',
+        timestamp: '2d',
+        action: 'created collection CHS',
+        content: 'Papers being considered for the bibliography of project CHS.',
+      },
+    ],
+  },
+  E.right,
+)
+
--- src/views/instantiate.ts
+++ src/views/instantiate.ts
@@ -2,12 +2,29 @@ import { getAbout } from './get-about'
 import { getCollection } from './get-collection'
 import { getCollections } from './get-collections'
 import { getEntry } from './get-entry'
+import { getLocalTimeline } from './get-local-timeline'
 import { ViewPath } from '../http/index.open'
 import { Queries } from '../readmodels'
 
 export const instantiate = (queries: Queries): ReadonlyArray<ViewPath> => [
-  { path: '/about', view: getAbout() },
-  { path: '/collections', view: getCollections(queries) },
-  { path: '/collections/:id', view: getCollection(queries) },
-  { path: '/entries/:id', view: getEntry(queries) },
+  {
+    path: '/about',
+    view: getAbout(),
+  },
+  {
+    path: '/collections',
+    view: getCollections(queries),
+  },
+  {
+    path: '/collections/:id',
+    view: getCollection(queries),
+  },
+  {
+    path: '/entries/:id',
+    view: getEntry(queries),
+  },
+  {
+    path: '/timelines/local',
+    view: getLocalTimeline(),
+  },
 ]
