bb796477aa9c1e85a76b9da0f90624216adc3718
Kevin Rutherford <kevin@rutherford-software.com>
2024-03-23 17:45:59 +0000

    Add frontMatter to the entry query #44

--- src/readmodels/entries/handle-event.ts
+++ src/readmodels/entries/handle-event.ts
@@ -2,12 +2,22 @@ import { Readmodel } from './readmodel'
 import { DomainEvent } from '../domain-event'
 
 export const handleEvent = (state: Readmodel) => (event: DomainEvent): void => {
-  if (event.type === 'doi-entered') {
-    const data = event.data
-    const current = state.byCollection.get(data.collectionId) ?? []
-    current.push(data)
-    state.byCollection.set(data.collectionId, current)
-    state.byEntryId.set(data.id, data)
+  switch (event.type) {
+    case 'doi-entered':
+      const data = event.data
+      const current = state.byCollection.get(data.collectionId) ?? []
+      current.push(data)
+      state.byCollection.set(data.collectionId, current)
+      state.byEntryId.set(data.id, data)
+      return
+    case 'front-matter-added':
+      const payload = event.data
+      const x = state.byEntryId.get(payload.entryId)
+      if (x)
+        x.frontMatter = payload.frontMatter
+      return
+    default:
+      return
   }
 }
 
