6ec41d3572ae55df68006be2e88227d10106a092
Kevin Rutherford <kevin@rutherford-software.com>
2024-03-23 14:10:54 +0000

    Restrict the event types going into the timeline readmodel #44

--- src/readmodels/local-timeline/handle-event.ts
+++ src/readmodels/local-timeline/handle-event.ts
@@ -2,6 +2,12 @@ import { Readmodel } from './readmodel'
 import { DomainEvent } from '../domain-event'
 
 export const handleEvent = (state: Readmodel) => (event: DomainEvent): void => {
-  state.push(event)
+  const timelineEventTypes = [
+    'collection-created',
+    'doi-entered',
+    'comment-created',
+  ]
+  if (timelineEventTypes.includes(event.type))
+    state.push(event)
 }
 
