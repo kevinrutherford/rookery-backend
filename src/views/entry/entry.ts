8c68a5262f540d7a093d229bc138d84a50722fd7
Kevin Rutherford <kevin@rutherford-software.com>
2024-03-29 17:39:02 +0000

    Allow the client to request optional inclusions when fetching an entry #57

--- src/views/entry/entry.ts
+++ src/views/entry/entry.ts
@@ -4,14 +4,27 @@ import * as RA from 'fp-ts/ReadonlyArray'
 import * as T from 'fp-ts/Task'
 import { pipe } from 'fp-ts/function'
 import * as t from 'io-ts'
+import { optionFromNullable } from 'io-ts-types'
 import { renderEntry } from './render-entry'
 import { View } from '../../http/index.open'
 import { Queries } from '../../readmodels'
 import { renderCollection } from '../collection/render-collection'
 import { validateInput } from '../validate-input'
 
+const csv = new t.Type<Array<string>, string, unknown>(
+  'CommaSeparatedValueCodec',
+  (input): input is Array<string> =>
+    Array.isArray(input) && input.every((value) => typeof value === 'string'),
+  (input, context) => pipe(
+    t.string.validate(input, context),
+    E.chain((str) => t.success(str.split(','))),
+  ),
+  (output: Array<string>) => output.join(','),
+)
+
 const paramsCodec = t.type({
   id: t.string,
+  include: optionFromNullable(csv),
 })
 
 export const getEntry = (queries: Queries): View => (input) => pipe(
