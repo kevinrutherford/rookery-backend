5d85a22fafb4d1b31f903cbcd89af8166b9023e8
Kevin Rutherford <kevin@rutherford-software.com>
2024-04-03 11:18:47 +0100

    create seams to allow duplicated resource identifiers to be OAOO #57

--- src/views/entry/render-entry.ts
+++ src/views/entry/render-entry.ts
@@ -1,20 +1,35 @@
 import { Entry } from '../../readmodels/entries/entry'
 
+type ResourceIdentifier = {
+  type: string,
+  id: string,
+}
+
+const renderCollectionIdentifier = (id: ResourceIdentifier['id']): ResourceIdentifier => ({
+  type: 'collection',
+  id,
+})
+
+const renderEntryIdentifier = (id: ResourceIdentifier['id']): ResourceIdentifier => ({
+  type: 'entry',
+  id,
+})
+
+const renderWorkIdentifier = (id: ResourceIdentifier['id']): ResourceIdentifier => ({
+  type: 'work',
+  id,
+})
+
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export const renderEntry = (entry: Entry) => ({
-  type: 'entry',
-  id: entry.id,
+  ...renderEntryIdentifier(entry.id),
   attributes: {
     addedAt: entry.addedAt.toISOString(),
   },
   relationships: {
-    collection: {
-      type: 'collection',
-      id: entry.collectionId,
-    },
+    collection: renderCollectionIdentifier(entry.collectionId),
     work: {
-      type: 'work',
-      id: entry.workId,
+      ...renderWorkIdentifier(entry.workId),
     },
   },
 })
