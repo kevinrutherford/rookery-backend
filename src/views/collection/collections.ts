fa3601144fcfd5306f038e17d6dd38c0d193edf4
Kevin Rutherford <kevin@rutherford-software.com>
2024-04-03 11:59:43 +0100

    Return standard collection resources from /collections #62

--- src/views/collection/collections.ts
+++ src/views/collection/collections.ts
@@ -1,10 +1,13 @@
+import * as RA from 'fp-ts/ReadonlyArray'
 import * as TE from 'fp-ts/TaskEither'
 import { pipe } from 'fp-ts/function'
+import { renderCollection } from './render-collection'
 import { View } from '../../http/index.open'
 import { Queries } from '../../readmodels'
 
 export const getCollections = (queries: Queries): View => () => pipe(
   queries.allCollections(),
+  RA.map(renderCollection),
   (collections) => ({
     data: collections,
   }),
