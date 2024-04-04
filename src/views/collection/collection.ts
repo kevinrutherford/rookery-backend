8b35f0d69956bcd64232d72ac8756f5670b04a47
Kevin Rutherford <kevin@rutherford-software.com>
2024-04-04 19:26:29 +0100

    Allow the client to request certain included documents #62

--- src/views/collection/collection.ts
+++ src/views/collection/collection.ts
@@ -3,14 +3,37 @@ import * as RA from 'fp-ts/ReadonlyArray'
 import * as T from 'fp-ts/Task'
 import { pipe } from 'fp-ts/function'
 import * as t from 'io-ts'
+import { optionFromNullable } from 'io-ts-types'
 import { renderCollection } from './render-collection'
 import { View } from '../../http/index.open'
 import { Queries } from '../../readmodels'
 import { renderEntry } from '../entry/render-entry'
 import { validateInput } from '../validate-input'
 
+const includes = t.union([
+  t.literal('entries'),
+  t.literal('entries.work'),
+])
+
+type Includes = t.TypeOf<typeof includes>
+
+const csv = new t.Type<ReadonlyArray<Includes>, string, unknown>(
+  'CommaSeparatedValueCodec',
+  (input): input is Array<Includes> => (
+    Array.isArray(input)
+    && input.every((value) => typeof value === 'string' && includes.is(value))
+  ),
+  (input, context) => pipe(
+    t.string.validate(input, context),
+    E.map((str) => str.split(',')),
+    E.chain(E.traverseArray((v) => includes.validate(v, context))),
+  ),
+  (output: ReadonlyArray<Includes>) => output.join(','),
+)
+
 const paramsCodec = t.type({
   id: t.string,
+  include: optionFromNullable(csv),
 })
 
 export const getCollection = (queries: Queries): View => (input) => pipe(
