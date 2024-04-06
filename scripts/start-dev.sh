eeaec74b1ef546a51a3d873e1942f4c44df48520
Kevin Rutherford <kevin@rutherford-software.com>
2024-04-06 14:17:01 +0100

    Allow the routes to be debugged #62

--- scripts/start-dev.sh
+++ scripts/start-dev.sh
@@ -1,7 +1,8 @@
 #! /bin/sh
 
-node_modules/.bin/ts-node-dev --exit-child --transpile-only --watch ./src ./src/index.ts &
+DEBUG=express:router:route node_modules/.bin/ts-node-dev --exit-child --transpile-only --watch ./src ./src/index.ts &
 pid=$!
 
 trap "echo 'Caught signal; killing watcher'; kill ${pid}; exit 1" INT QUIT TERM
 wait
+
