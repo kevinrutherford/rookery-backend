a4fdb8210619404b37d3b68ea959238440dd8665
Kevin Rutherford <kevin@rutherford-software.com>
2024-04-06 14:31:18 +0100

    Actually return the work from the readmodel #62

--- src/readmodels/works/lookup-work.ts
+++ src/readmodels/works/lookup-work.ts
@@ -1,13 +1,20 @@
+import * as O from 'fp-ts/Option'
+import * as RM from 'fp-ts/ReadonlyMap'
+import { pipe } from 'fp-ts/function'
+import * as S from 'fp-ts/string'
 import { Readmodel } from './readmodel'
 import { Work } from './work'
 
 type LookupWork = (currentState: Readmodel) => (id: string) => Work
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const lookupWork: LookupWork = (currentState) => (id) => ({
-  id,
-  frontMatter: {
-    crossrefStatus: 'not-determined',
-  },
-})
+export const lookupWork: LookupWork = (currentState) => (id) => pipe(
+  currentState,
+  RM.lookup(S.Eq)(id),
+  O.getOrElseW(() => ({
+    id,
+    frontMatter: {
+      crossrefStatus: 'not-determined' as const,
+    },
+  })),
+)
 
