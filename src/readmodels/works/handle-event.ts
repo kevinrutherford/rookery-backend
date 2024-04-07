46d052748f87d51601f132358f7c10baf415fe88
Kevin Rutherford <kevin@rutherford-software.com>
2024-04-07 19:16:41 +0100

    Extend to include all works that have been entered #64

--- src/readmodels/works/handle-event.ts
+++ src/readmodels/works/handle-event.ts
@@ -2,7 +2,17 @@ import { Readmodel } from './readmodel'
 import { DomainEvent } from '../domain-event'
 
 export const handleEvent = (state: Readmodel) => (event: DomainEvent): void => {
-  if (event.type === 'work-updated') {
+  if (event.type === 'doi-entered') {
+    const existing = state.get(event.data.workId)
+    if (!existing) {
+      state.set(event.data.workId, {
+        id: event.data.workId,
+        frontMatter: {
+          crossrefStatus: 'not-determined',
+        },
+      })
+    }
+  } else if (event.type === 'work-updated') {
     state.set(event.data.workId, {
       id: event.data.workId,
       frontMatter: event.data.attributes,
