669ce8ead209997d1a3cd91e5d301a21c026c446
Kevin Rutherford <kevin@rutherford-software.com>
2024-04-08 11:40:40 +0100

    Allow the works to be filtered by crossrefStatus #64

--- src/views/work/works.ts
+++ src/views/work/works.ts
@@ -1,16 +1,45 @@
+import * as O from 'fp-ts/Option'
 import * as RA from 'fp-ts/ReadonlyArray'
 import * as TE from 'fp-ts/TaskEither'
 import { pipe } from 'fp-ts/function'
+import * as t from 'io-ts'
+import * as tt from 'io-ts-types'
 import { renderWork } from './render-work'
 import { View } from '../../http/index.open'
 import { Queries } from '../../readmodels'
+import { Work } from '../../readmodels/works/work'
+import { validateInput } from '../validate-input'
 
-export const getWorks = (queries: Queries): View => () => pipe(
-  queries.allWorks(),
-  RA.map(renderWork),
-  (works) => ({
-    data: works,
+const paramsCodec = t.type({
+  filter: t.type({
+    crossrefStatus: tt.optionFromNullable(t.string),
   }),
-  TE.right,
+})
+
+type Params = t.TypeOf<typeof paramsCodec>
+
+const renderResults = (works: ReadonlyArray<Work>) => pipe(
+  works,
+  RA.map(renderWork),
+  (resources) => ({ data: resources }),
+)
+
+const selectWorks = (queries: Queries) => (params: Params) => pipe(
+  params.filter.crossrefStatus,
+  O.match(
+    () => queries.allWorks(),
+    (v) => pipe(
+      queries.allWorks(),
+      RA.filter((w) => w.frontMatter.crossrefStatus === v),
+    ),
+  ),
+)
+
+export const getWorks = (queries: Queries): View => (input: unknown) => pipe(
+  input,
+  validateInput(paramsCodec),
+  TE.fromEither,
+  TE.map(selectWorks(queries)),
+  TE.map(renderResults),
 )
 
