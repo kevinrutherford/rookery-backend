24b4e1d2eb7ed3fe50896490b008b1b22a373a02
Kevin Rutherford <kevin@rutherford-software.com>
2024-04-18 15:00:13 +0100

    Provide a Community resource with a singleton id #78

--- src/views/community/community.ts
+++ src/views/community/community.ts
@@ -2,21 +2,18 @@ import * as TE from 'fp-ts/TaskEither'
 import { pipe } from 'fp-ts/function'
 import { View } from '../../http/index.open'
 
-export const getAbout = (): View => () => pipe(
+export const getCommunity = (): View => () => pipe(
   {
     data: {
       type: 'community',
-      community: {
+      id: '1',
+      attributes: {
         name: 'Health organisation, policy and economics (HOPE)',
         affiliation: 'Centre for Primary Care and Health Services Research, Manchester University',
         overview: `
         This interdisciplinary theme focuses upon research which investigates the supply, organisation, management and financing of health and social care services.
 
 Our expertise encompasses rigorous econometric analysis and a wide range of qualitative social scientific methods, including particular experience in the use of ethnographic approaches to understand organisational processes.`,
-        admins: ['@DonnaB', '@kevin'],
-      },
-      backend: {
-        version: 'unknown',
       },
     },
   },
--- src/views/instantiate.ts
+++ src/views/instantiate.ts
@@ -1,6 +1,6 @@
 import { getCollection } from './collection/collection'
 import { getCollections } from './collection/collections'
-import { getAbout } from './community/about'
+import { getCommunity } from './community/community'
 import { getEntries } from './entry/entries'
 import { getEntry } from './entry/entry'
 import { getLocalTimeline } from './timeline/local'
@@ -11,8 +11,8 @@ import { Queries } from '../readmodels'
 export const instantiate = (queries: Queries): ReadonlyArray<ViewPath> => {
   return [
     {
-      path: '/about',
-      view: getAbout(),
+      path: '/community',
+      view: getCommunity(),
     },
     {
       path: '/collections',
