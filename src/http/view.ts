f23c8d88249132eed0841749c54c5c120b5221a1
Kevin Rutherford <kevin@rutherford-software.com>
2024-05-31 10:48:11 +0100

    Record a strategic decision

--- src/http/view.ts
+++ src/http/view.ts
@@ -1,11 +1,6 @@
 import * as TE from 'fp-ts/TaskEither'
 import { ErrorOutcome } from './error-outcome'
-import { JsonApiResource } from '../views/json-api-resource'
-
-type JsonApiDocument = {
-  data: JsonApiResource | ReadonlyArray<JsonApiResource>,
-  included?: ReadonlyArray<JsonApiResource>,
-}
+import { JsonApiDocument } from '../views/json-api-resource'
 
 export type View = (isAuthenticated: boolean) => (input: unknown) => TE.TaskEither<ErrorOutcome, JsonApiDocument>
 
--- src/views/json-api-resource.ts
+++ src/views/json-api-resource.ts
@@ -1,6 +1,9 @@
 import { Json } from 'fp-ts/Json'
 import { ResourceIdentifier } from './resource-identifier'
 
+/**
+ * @deprecated Switch to JSON-LD to align with the rest of the fediverse
+ */
 export type JsonApiResource = ResourceIdentifier & {
   attributes: Json,
   relationships?: Record<string, {
@@ -9,5 +12,13 @@ export type JsonApiResource = ResourceIdentifier & {
       related: string,
     },
   }>,
+}
+
+/**
+ * @deprecated Switch to JSON-LD to align with the rest of the fediverse
+ */
+export type JsonApiDocument = {
+  data: JsonApiResource | ReadonlyArray<JsonApiResource>,
+  included?: ReadonlyArray<JsonApiResource>,
 }
 
