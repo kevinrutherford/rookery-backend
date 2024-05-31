c889eeed6cbc3b4baf4e1f58908d0c026eb3f95e
Kevin Rutherford <kevin@rutherford-software.com>
2024-05-31 11:09:37 +0100

    Specify timeline behaviour relative to private collections #103

---
+++ test/domain-model/private-collections.test.ts
@@ -0,0 +1,14 @@
+describe('private collections', () => {
+  describe('when a public collection becomes private', () => {
+    it.todo('all earlier activities remain public')
+
+    it.todo('all future activities are private')
+  })
+
+  describe('when a private collection becomes public', () => {
+    it.todo('all earlier activities remain private')
+
+    it.todo('all future activities are public')
+  })
+})
+
