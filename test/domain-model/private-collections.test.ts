e7484539c740c6a011933c2d4804424acd7b562d
Kevin Rutherford <kevin@rutherford-software.com>
2024-05-31 12:25:19 +0100

    Cobble together a test implementation by duplicating EventStore code #103

--- test/domain-model/private-collections.test.ts
+++ test/domain-model/private-collections.test.ts
@@ -1,6 +1,44 @@
+import { getLocalTimeline } from '../../src/domain-model/local-timeline/get-local-timeline'
+import { handleEvent } from '../../src/domain-model/local-timeline/handle-event'
+import { Readmodel } from '../../src/domain-model/local-timeline/readmodel'
+
 describe('private collections', () => {
   describe('when a public collection becomes private', () => {
-    it.todo('all earlier activities remain public')
+    const model: Readmodel = []
+    handleEvent(model)({
+      streamId: '1',
+      id: '1',
+      created: new Date(),
+      isJson: true,
+      revision: BigInt(1),
+      type: 'collection-created',
+      data: {
+        id: 'abc',
+        name: 'abc',
+        description: 'abc',
+      },
+      metadata: undefined,
+    })
+    handleEvent(model)({
+      streamId: '1',
+      id: '1',
+      created: new Date(),
+      isJson: true,
+      revision: BigInt(2),
+      type: 'collection-updated',
+      data: {
+        collectionId: 'abc',
+        attributes: {
+          isPrivate: true,
+        },
+      },
+      metadata: undefined,
+    })
+    const activities = getLocalTimeline(model)(true)
+
+    it('all earlier activities remain public', () => {
+      expect(activities[0].isPrivate).toBe(false)
+    })
 
     it.todo('all future activities are private')
   })
