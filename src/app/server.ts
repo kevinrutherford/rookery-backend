8b4384793e9e0fc756e3cc49edf68f00518369af
Kevin Rutherford <kevin@rutherford-software.com>
2024-06-01 16:57:27 +0100

    Clean up a rename #103

--- src/app/server.ts
+++ src/app/server.ts
@@ -1,9 +1,9 @@
-import * as Readmodels from '../domain-model'
+import * as DomainModel from '../domain-model'
 import { createHttpServer } from '../http'
 import * as Views from '../views'
 
 export const makeServer = async (): Promise<void> => {
-  const queries = Readmodels.instantiate()
+  const queries = DomainModel.instantiate()
   const views = Views.instantiate(queries)
 
   createHttpServer(views)
