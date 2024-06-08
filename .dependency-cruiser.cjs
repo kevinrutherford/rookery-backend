974e34bd8a8bbd9fbc0797b5e57edb3e840fe0a8
Kevin Rutherford <kevin@rutherford-software.com>
2024-06-08 19:34:09 +0100

    Summarise some folders in the graphs

--- .dependency-cruiser.cjs
+++ .dependency-cruiser.cjs
@@ -6,8 +6,8 @@ module.exports = {
       name: 'no-circular',
       severity: 'error',
       comment:
-        'This dependency is part of a circular relationship. You might want to revise ' +
-        'your solution (i.e. use dependency inversion, make sure the modules have a single responsibility) ',
+        'This dependency is part of a circular relationship. You might want to revise '
+        + 'your solution (i.e. use dependency inversion, make sure the modules have a single responsibility) ',
       from: {},
       to: {
         circular: true,
@@ -16,11 +16,11 @@ module.exports = {
     {
       name: 'no-orphans',
       comment:
-        'This is an orphan module - it\'s likely not used (anymore?). Either use it or ' +
-        'remove it. If it\'s logical this module is an orphan (i.e. it\'s a config file), ' +
-        'add an exception for it in your dependency-cruiser configuration. By default ' +
-        'this rule does not scrutinize dot-files (e.g. .eslintrc.js), TypeScript declaration ' +
-        'files (.d.ts), tsconfig.json and some of the babel and webpack configs.',
+        'This is an orphan module - it\'s likely not used (anymore?). Either use it or '
+        + 'remove it. If it\'s logical this module is an orphan (i.e. it\'s a config file), '
+        + 'add an exception for it in your dependency-cruiser configuration. By default '
+        + 'this rule does not scrutinize dot-files (e.g. .eslintrc.js), TypeScript declaration '
+        + 'files (.d.ts), tsconfig.json and some of the babel and webpack configs.',
       severity: 'warn',
       from: {
         orphan: true,
@@ -36,8 +36,8 @@ module.exports = {
     {
       name: 'no-deprecated-core',
       comment:
-        'A module depends on a node core module that has been deprecated. Find an alternative - these are ' +
-        'bound to exist - node doesn\'t deprecate lightly.',
+        'A module depends on a node core module that has been deprecated. Find an alternative - these are '
+        + 'bound to exist - node doesn\'t deprecate lightly.',
       severity: 'warn',
       from: {},
       to: {
@@ -71,8 +71,8 @@ module.exports = {
     {
       name: 'not-to-deprecated',
       comment:
-        'This module uses a (version of an) npm module that has been deprecated. Either upgrade to a later ' +
-        'version of that module, or find an alternative. Deprecated modules are a security risk.',
+        'This module uses a (version of an) npm module that has been deprecated. Either upgrade to a later '
+        + 'version of that module, or find an alternative. Deprecated modules are a security risk.',
       severity: 'warn',
       from: {},
       to: {
@@ -85,10 +85,10 @@ module.exports = {
       name: 'no-non-package-json',
       severity: 'error',
       comment:
-        'This module depends on an npm package that isn\'t in the \'dependencies\' section of your package.json. ' +
-        'That\'s problematic as the package either (1) won\'t be available on live (2 - worse) will be ' +
-        'available on live with an non-guaranteed version. Fix it by adding the package to the dependencies ' +
-        'in your package.json.',
+        'This module depends on an npm package that isn\'t in the \'dependencies\' section of your package.json. '
+        + 'That\'s problematic as the package either (1) won\'t be available on live (2 - worse) will be '
+        + 'available on live with an non-guaranteed version. Fix it by adding the package to the dependencies '
+        + 'in your package.json.',
       from: {},
       to: {
         dependencyTypes: [
@@ -100,8 +100,8 @@ module.exports = {
     {
       name: 'not-to-unresolvable',
       comment:
-        'This module depends on a module that cannot be found (\'resolved to disk\'). If it\'s an npm ' +
-        'module: add it to your package.json. In all other cases you likely already know what to do.',
+        'This module depends on a module that cannot be found (\'resolved to disk\'). If it\'s an npm '
+        + 'module: add it to your package.json. In all other cases you likely already know what to do.',
       severity: 'error',
       from: {
         pathNot: '\\.d\\.ts$',
@@ -113,9 +113,9 @@ module.exports = {
     {
       name: 'no-duplicate-dep-types',
       comment:
-        'Likely this module depends on an external (\'npm\') package that occurs more than once ' +
-        'in your package.json i.e. bot as a devDependencies and in dependencies. This will cause ' +
-        'maintenance problems later on.',
+        'Likely this module depends on an external (\'npm\') package that occurs more than once '
+        + 'in your package.json i.e. bot as a devDependencies and in dependencies. This will cause '
+        + 'maintenance problems later on.',
       severity: 'warn',
       from: {},
       to: {
@@ -131,9 +131,9 @@ module.exports = {
     {
       name: 'not-to-spec',
       comment:
-        'This module depends on a spec (test) file. The sole responsibility of a spec file is to test code. ' +
-        'If there\'s something in a spec that\'s of use to other modules, it doesn\'t have that single ' +
-        'responsibility anymore. Factor it out into (e.g.) a separate utility/ helper or a mock.',
+        'This module depends on a spec (test) file. The sole responsibility of a spec file is to test code. '
+        + 'If there\'s something in a spec that\'s of use to other modules, it doesn\'t have that single '
+        + 'responsibility anymore. Factor it out into (e.g.) a separate utility/ helper or a mock.',
       severity: 'error',
       from: {},
       to: {
@@ -144,11 +144,11 @@ module.exports = {
       name: 'not-to-dev-dep',
       severity: 'error',
       comment:
-        'This module depends on an npm package from the \'devDependencies\' section of your ' +
-        'package.json. It looks like something that ships to production, though. To prevent problems ' +
-        'with npm packages that aren\'t there on production declare it (only!) in the \'dependencies\'' +
-        'section of your package.json. If this module is development only - add it to the ' +
-        'from.pathNot re of the not-to-dev-dep rule in the dependency-cruiser configuration',
+        'This module depends on an npm package from the \'devDependencies\' section of your '
+        + 'package.json. It looks like something that ships to production, though. To prevent problems '
+        + 'with npm packages that aren\'t there on production declare it (only!) in the \'dependencies\''
+        + 'section of your package.json. If this module is development only - add it to the '
+        + 'from.pathNot re of the not-to-dev-dep rule in the dependency-cruiser configuration',
       from: {
         path: '^(src)',
         pathNot: '\\.(spec|test)\\.(js|mjs|cjs|ts|ls|coffee|litcoffee|coffee\\.md)$|\\.d\\.ts$',
@@ -163,10 +163,10 @@ module.exports = {
       name: 'optional-deps-used',
       severity: 'info',
       comment:
-        'This module depends on an npm package that is declared as an optional dependency ' +
-        'in your package.json. As this makes sense in limited situations only, it\'s flagged here. ' +
-        'If you\'re using an optional dependency here by design - add an exception to your' +
-        'dependency-cruiser configuration.',
+        'This module depends on an npm package that is declared as an optional dependency '
+        + 'in your package.json. As this makes sense in limited situations only, it\'s flagged here. '
+        + 'If you\'re using an optional dependency here by design - add an exception to your'
+        + 'dependency-cruiser configuration.',
       from: {},
       to: {
         dependencyTypes: [
@@ -177,10 +177,10 @@ module.exports = {
     {
       name: 'peer-deps-used',
       comment:
-        'This module depends on an npm package that is declared as a peer dependency ' +
-        'in your package.json. This makes sense if your package is e.g. a plugin, but in ' +
-        'other cases - maybe not so much. If the use of a peer dependency is intentional ' +
-        'add an exception to your dependency-cruiser configuration.',
+        'This module depends on an npm package that is declared as a peer dependency '
+        + 'in your package.json. This makes sense if your package is e.g. a plugin, but in '
+        + 'other cases - maybe not so much. If the use of a peer dependency is intentional '
+        + 'add an exception to your dependency-cruiser configuration.',
       severity: 'warn',
       from: {},
       to: {
@@ -192,7 +192,7 @@ module.exports = {
   ],
   options: {
     doNotFollow: { path: 'node_modules' },
-    exclude : {
+    exclude: {
       path: [
         '^.*-spec.ts$',
         'helper',
@@ -200,7 +200,7 @@ module.exports = {
       ],
       dynamic: true,
     },
-    includeOnly : '^src/',
+    includeOnly: '^src/',
     tsPreCompilationDeps: true,
     tsConfig: { fileName: 'tsconfig.json' },
     enhancedResolveOptions: {
@@ -277,7 +277,7 @@ module.exports = {
           ],
         },
       },
-      'text': { 'highlightFocused': true },
+      text: { highlightFocused: true },
     },
   },
 }
