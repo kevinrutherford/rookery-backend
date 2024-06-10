d6cbbf9d9a801e13b186f1f5e42159cf8006fbe2
Kevin Rutherford <kevin@rutherford-software.com>
2024-06-10 16:54:20 +0100

    Define the type of findEntries in the client #103

--- src/domain-model/entries/find-entries.ts
+++ src/domain-model/entries/find-entries.ts
@@ -2,10 +2,10 @@ import * as O from 'fp-ts/Option'
 import * as RM from 'fp-ts/ReadonlyMap'
 import { identity, pipe } from 'fp-ts/function'
 import * as S from 'fp-ts/string'
-import { Entry } from './entry'
 import { Readmodel } from './readmodel'
+import { Queries } from '../../views/queries'
 
-export const findEntries = (currentState: Readmodel) => (collectionId: string): ReadonlyArray<Entry> => pipe(
+export const findEntries = (currentState: Readmodel): Queries['findEntries'] => (collectionId) => pipe(
   currentState.byCollection,
   RM.lookup(S.Eq)(collectionId),
   O.match(
