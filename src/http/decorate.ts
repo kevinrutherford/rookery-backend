c59bc651a1857aed251b2832327fc2b4d6c37629
Kevin Rutherford <kevin@rutherford-software.com>
2024-06-10 14:08:01 +0100

    Create another (unused) authority decorator #103

--- src/http/decorate.ts
+++ src/http/decorate.ts
@@ -1,10 +1,22 @@
+import * as E from 'fp-ts/Either'
 import * as RA from 'fp-ts/ReadonlyArray'
 import { pipe } from 'fp-ts/function'
 import { Authority } from '../auth/authority'
 import { Queries } from '../domain-model'
+import { Collection } from '../domain-model/collections/collection'
 
 export const allCollections = (queries: Queries) => (clientCan: Authority): Queries['allCollections'] => () => pipe(
   queries.allCollections(),
   RA.filter((c) => !c.isPrivate || clientCan('browse-private-collections')),
+)
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const lookupCollection = (queries: Queries) => (clientCan: Authority) => (collectionId: string): E.Either<'not-found' | 'not-authorised', Collection> => pipe(
+  collectionId,
+  queries.lookupCollection,
+  E.filterOrElseW(
+    (collection) => !collection.isPrivate || clientCan('browse-private-collections'),
+    () => 'not-authorised' as const,
+  ),
 )
 
