a0e0b6d8d34bf985eebda1627a9e5c8eefe0918d
Kevin Rutherford <kevin@rutherford-software.com>
2024-06-12 17:54:27 +0100

    Extract another resource definition #103

---
+++ src/services/work-resource.ts
@@ -0,0 +1,24 @@
+type FrontMatterNotDetermined = {
+  crossrefStatus: 'not-determined',
+  reason: 'never-fetched' | 'response-invalid' | 'response-unavailable',
+}
+
+type FrontMatterNotFound = {
+  crossrefStatus: 'not-found',
+}
+
+type FrontMatterFound = {
+  crossrefStatus: 'found',
+  title: string,
+  abstract: string,
+  authors: ReadonlyArray<string>,
+}
+
+type FrontMatter = FrontMatterFound | FrontMatterNotFound | FrontMatterNotDetermined
+
+export type Work = {
+  id: string,
+  updatedAt: Date,
+  frontMatter: FrontMatter,
+}
+
