a6d270b89c914f1bed3fd96c2599dcdd1ca9827b
Kevin Rutherford <kevin@rutherford-software.com>
2024-06-17 14:18:48 +0100

    Implement the auth check in only one place #113

--- src/restricted-domain/decorate.ts
+++ src/restricted-domain/decorate.ts
@@ -2,18 +2,23 @@ import * as E from 'fp-ts/Either'
 import * as RA from 'fp-ts/ReadonlyArray'
 import { pipe } from 'fp-ts/function'
 import { Authority } from '../auth/authority'
+import { Collection } from '../services/collection-resource'
 import { Domain } from '../services/domain'
 
-export const allCollections = (queries: Domain, clientCan: Authority): Domain['allCollections'] => () => pipe(
-  queries.allCollections(),
-  RA.filter((c) => !c.isPrivate || clientCan('browse-private-collections')),
+const clientCanAccessCollection = (clientCan: Authority) => (collection: Collection) => (
+  !collection.isPrivate || clientCan('browse-private-collections')
 )
 
-export const lookupCollection = (queries: Domain, clientCan: Authority): Domain['lookupCollection'] => (collectionId) => pipe(
+export const allCollections = (queries: Domain, claims: Authority): Domain['allCollections'] => () => pipe(
+  queries.allCollections(),
+  RA.filter(clientCanAccessCollection(claims)),
+)
+
+export const lookupCollection = (queries: Domain, claims: Authority): Domain['lookupCollection'] => (collectionId) => pipe(
   collectionId,
   queries.lookupCollection,
   E.filterOrElseW(
-    (collection) => !collection.isPrivate || clientCan('browse-private-collections'),
+    clientCanAccessCollection(claims),
     () => 'not-authorised' as const,
   ),
 )
