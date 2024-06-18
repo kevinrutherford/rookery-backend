fc331dcdcf92858ea8c76f64c81adcc913509e53
Kevin Rutherford <kevin@rutherford-software.com>
2024-06-18 09:03:23 +0100

    Set the context only once #117

--- src/http/invoke-service.ts
+++ src/http/invoke-service.ts
@@ -1,5 +1,6 @@
 import { Middleware } from '@koa/router'
 import * as E from 'fp-ts/Either'
+import { pipe } from 'fp-ts/function'
 import { StatusCodes } from 'http-status-codes'
 import * as Auth from '../auth'
 import { Logger } from '../logger'
@@ -26,20 +27,29 @@ export const invokeService: InvokeService = (logger, service, unrestrictedDomain
   const authority = Auth.instantiate(context.request.token)
   const restrictedDomain = RestrictedDomain.instantiate(authority, unrestrictedDomain)
 
+  const result = pipe(
+    service(restrictedDomain)(authority)({ ...context.params, ...context.query }),
+    E.matchW(
+      (errors) => {
+        errors.errors.forEach((error) => {
+          if (error.code === 'fatal-error')
+            logger.error(error.title, error.meta)
+          else
+            logger.debug(error.title, error.meta)
+        })
+        return ({
+          status: errorToStatus(errors),
+          body: errors,
+        })
+      },
+      (body) => ({
+        status: StatusCodes.OK,
+        body,
+      }),
+    ),
+  )
   context.response.type = 'json'
-  const response = service(restrictedDomain)(authority)({ ...context.params, ...context.query })
-  if (E.isRight(response)) {
-    context.response.status = StatusCodes.OK
-    context.response.body = response.right
-  } else {
-    response.left.errors.forEach((error) => {
-      if (error.code === 'fatal-error')
-        logger.error(error.title, error.meta)
-      else
-        logger.debug(error.title, error.meta)
-    })
-    context.response.status = errorToStatus(response.left)
-    context.response.body = response.left
-  }
+  context.response.status = result.status
+  context.response.body = result.body
 }
 
