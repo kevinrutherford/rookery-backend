71057123638af9b13cdbaf54eef4d1b5e0003306
Kevin Rutherford <kevin@rutherford-software.com>
2024-06-20 08:10:10 +0100

    Reastrict access to entries in private collections #114

--- src/restricted-domain/decorate.ts
+++ src/restricted-domain/decorate.ts
@@ -1,11 +1,21 @@
 import * as E from 'fp-ts/Either'
+import * as O from 'fp-ts/Option'
 import * as RA from 'fp-ts/ReadonlyArray'
 import { pipe } from 'fp-ts/function'
 import { Authority } from '../auth/authority'
-import { Collection, Domain } from '../services/index.open'
+import { Collection, Domain, Entry } from '../services/index.open'
 
-const clientCanAccessCollection = (clientCan: Authority) => (collection: Collection) => (
+const clientCanAccessCollection = (clientCan: Authority) => (collection: Collection): boolean => (
   !collection.isPrivate || clientCan('browse-private-collections')
+)
+
+const clientCanAccessEntry = (queries: Domain, clientCan: Authority) => (entry: Entry): boolean => pipe(
+  entry.collectionId,
+  queries.lookupCollection,
+  E.match(
+    () => false, // SMELL: should be a fatal error
+    clientCanAccessCollection(clientCan),
+  ),
 )
 
 export const allCollections = (queries: Domain, claims: Authority): Domain['allCollections'] => () => pipe(
@@ -26,5 +36,6 @@ export const lookupCollection = (queries: Domain, claims: Authority): Domain['lo
 export const lookupEntry = (queries: Domain, claims: Authority): Domain['lookupEntry'] => (entryId) => pipe(
   entryId,
   queries.lookupEntry,
+  O.filter(clientCanAccessEntry(queries, claims)),
 )
 
--- src/services/index.open.ts
+++ src/services/index.open.ts
@@ -1,3 +1,4 @@
 export { Domain } from './domain/domain'
 export { Collection } from './domain/collection-resource'
+export { Entry } from './domain/entry-resource'
 
