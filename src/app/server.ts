b7db36d13bbdf7eaa4f8a47a2915d0839c0222e5
Kevin Rutherford <kevin@rutherford-software.com>
2024-06-22 19:15:38 +0100

    Warn if events are received out of sequence #115

--- src/app/server.ts
+++ src/app/server.ts
@@ -9,6 +9,8 @@ import { DomainObserver } from '../unrestricted-domain/index.open'
 const reportEvents = (logger: Logger.Logger): DomainObserver => (domain: Domain) => {
   const info = domain.info()
   logger.info('Events handled', { count: info.eventsCount })
+  if (info.unexpectedEvents.length > 0)
+    logger.warn('Unexpected events received', { events: info.unexpectedEvents })
   if (info.unrecognisedEvents.length > 0) {
     logger.error('Unrecognised event; terminating')
     process.exit(1)
