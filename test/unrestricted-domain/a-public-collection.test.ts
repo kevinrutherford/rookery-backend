0bb48b885ca71acd152d16e30b154a23963306f7
Kevin Rutherford <kevin@rutherford-software.com>
2024-06-23 19:14:40 +0100

    Complete more characterisation tests #115

--- test/unrestricted-domain/a-public-collection.test.ts
+++ test/unrestricted-domain/a-public-collection.test.ts
@@ -1,26 +1,70 @@
+import { Domain } from '../../src/domain/index.open'
 import * as UnrestrictedDomain from '../../src/unrestricted-domain'
 import { defaultTestObserver } from '../default-test-observer'
 import { arbitraryString, arbitraryWord } from '../helpers'
 import { mkEvent } from '../mk-event'
 
 describe('given a public collection', () => {
-  describe('doi-entered', () => {
+  const workId = arbitraryWord()
+
+  let d: Domain
+  let h: UnrestrictedDomain.EventHandler
+
+  beforeEach(() => {
     const { domain, handleEvent } = UnrestrictedDomain.instantiate(defaultTestObserver)
+    d = domain
+    h = handleEvent
+  })
 
-    const collectionId = arbitraryWord()
-    handleEvent(mkEvent('collection-created', {
-      id: collectionId,
-      name: arbitraryString(),
-      description: arbitraryString(),
-    }))
-    handleEvent(mkEvent('doi-entered', {
-      id: arbitraryWord(),
-      workId: arbitraryWord(),
-      collectionId,
-    }))
+  describe('and no Works', () => {
+    describe('doi-entered', () => {
+      beforeEach(() => {
+        const collectionId = arbitraryWord()
+        h(mkEvent('collection-created', {
+          id: collectionId,
+          name: arbitraryString(),
+          description: arbitraryString(),
+        }))
+        const doiEntered = mkEvent('doi-entered', {
+          id: arbitraryWord(),
+          workId,
+          collectionId,
+        })
+        h(doiEntered)
+      })
 
-    it('records the activity', () => {
-      expect(domain.getLocalTimeline()).toHaveLength(2)
+      it('records the activity', () => {
+        expect(d.getLocalTimeline()).toHaveLength(2)
+      })
+
+      it('records the new Work', () => {
+        expect(d.allWorks()).toHaveLength(1)
+        expect(d.allWorks()[0].id).toStrictEqual(workId)
+      })
+    })
+  })
+
+  describe('and one pre-existing Work', () => {
+    describe('doi-entered', () => {
+
+      beforeEach(() => {
+        const collectionId = arbitraryWord()
+        h(mkEvent('collection-created', {
+          id: collectionId,
+          name: arbitraryString(),
+          description: arbitraryString(),
+        }))
+        const doiEntered = mkEvent('doi-entered', {
+          id: arbitraryWord(),
+          workId,
+          collectionId,
+        })
+        h(doiEntered)
+        h(doiEntered)
+      })
+      it('does not add a new Work', () => {
+        expect(d.allWorks()).toHaveLength(1)
+      })
     })
   })
 })
