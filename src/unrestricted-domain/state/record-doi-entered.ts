862b6d638c5b48f8fc1717582ab4c1bfc082327c
Kevin Rutherford <kevin@rutherford-software.com>
2024-06-23 18:50:05 +0100

    Convert a conditional to be a guard clause #115

--- src/unrestricted-domain/state/record-doi-entered.ts
+++ src/unrestricted-domain/state/record-doi-entered.ts
@@ -2,20 +2,20 @@ import { DoiEnteredEvent } from '../domain-event'
 import { Readmodel } from '../readmodel'
 
 export const recordDoiEntered = (state: Readmodel, event: DoiEnteredEvent): void => {
-  if (state.collections.get(event.data.collectionId) === undefined)
+  if (state.collections.get(event.data.collectionId) === undefined) {
     state.info.unexpectedEvents.push(event)
-  else {
-    const existing = state.works.get(event.data.workId)
-    if (!existing) {
-      state.works.set(event.data.workId, {
-        id: event.data.workId,
-        updatedAt: event.created,
-        frontMatter: {
-          crossrefStatus: 'not-determined',
-          reason: 'never-fetched',
-        },
-      })
-    }
+    return
+  }
+  const existing = state.works.get(event.data.workId)
+  if (!existing) {
+    state.works.set(event.data.workId, {
+      id: event.data.workId,
+      updatedAt: event.created,
+      frontMatter: {
+        crossrefStatus: 'not-determined',
+        reason: 'never-fetched',
+      },
+    })
   }
 }
 
