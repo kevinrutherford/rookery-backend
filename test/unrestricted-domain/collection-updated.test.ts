aea698f3d4e96e34202312648c1ec2dda317a3b1
Kevin Rutherford <kevin@rutherford-software.com>
2024-06-24 16:47:53 +0100

    Clarify the conditions under which tests currently fail #115

--- test/unrestricted-domain/collection-updated.test.ts
+++ test/unrestricted-domain/collection-updated.test.ts
@@ -4,36 +4,90 @@ import { arbitraryString, arbitraryWord } from '../helpers'
 import { mkEvent } from '../mk-event'
 
 describe('collection-updated', () => {
-  const { queries, handleEvent } = UnrestrictedDomain.instantiate(defaultTestObserver)
+  let domain: ReturnType<typeof UnrestrictedDomain.instantiate>
   const collectionId = arbitraryWord()
 
-  describe('when a public collection becomes private', () => {
-    handleEvent(mkEvent('collection-created', {
-      id: collectionId,
-      name: arbitraryString(),
-      description: arbitraryString(),
-    }))
-    handleEvent(mkEvent('doi-entered', {
+  const addEntry = () => {
+    domain.handleEvent(mkEvent('doi-entered', {
       id: arbitraryWord(),
       workId: arbitraryWord(),
       collectionId,
     }))
-    handleEvent(mkEvent('collection-updated', {
+  }
+
+  const becomePrivate = () => {
+    domain.handleEvent(mkEvent('collection-updated', {
       collectionId,
       attributes: {
         isPrivate: true,
       },
     }))
-    const activities = queries.getLocalTimeline()
+  }
 
-    it('all earlier activities remain public', () => {
+  const becomePublic = () => {
+    domain.handleEvent(mkEvent('collection-updated', {
+      collectionId,
+      attributes: {
+        isPrivate: false,
+      },
+    }))
+  }
+
+  beforeEach(() => {
+    domain = UnrestrictedDomain.instantiate(defaultTestObserver)
+    domain.handleEvent(mkEvent('collection-created', {
+      id: collectionId,
+      name: arbitraryString(),
+      description: arbitraryString(),
+    }))
+    addEntry()
+  })
+
+  describe('when a public collection becomes private', () => {
+    let activities: ReturnType<typeof domain.queries.getLocalTimeline>
+
+    beforeEach(() => {
+      becomePrivate()
+      addEntry()
+      activities = domain.queries.getLocalTimeline()
+    })
+
+    it('all activities are in the timeline', () => {
+      expect(activities).toHaveLength(3)
+    })
+
+    it('all activities during the public period remain public', () => {
       expect(activities[0].isPrivate).toBe(false)
       expect(activities[1].isPrivate).toBe(false)
+    })
+
+    it.failing('subsequent activities are private', () => {
+      expect(activities[2].isPrivate).toBe(true)
     })
   })
 
   describe('when a private collection becomes public', () => {
-    it.todo('all private activities remain private')
+    let activities: ReturnType<typeof domain.queries.getLocalTimeline>
+
+    beforeEach(() => {
+      becomePrivate()
+      addEntry()
+      becomePublic()
+      addEntry()
+      activities = domain.queries.getLocalTimeline()
+    })
+
+    it('all activities are in the timeline', () => {
+      expect(activities).toHaveLength(4)
+    })
+
+    it.failing('all activities during the private period remain private', () => {
+      expect(activities[2].isPrivate).toBe(true)
+    })
+
+    it('subsequent activities are public', () => {
+      expect(activities[3].isPrivate).toBe(false)
+    })
   })
 })
 
