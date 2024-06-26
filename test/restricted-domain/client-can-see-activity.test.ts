1132f8fedfd24b421b266d05b0a082c13a9a3be6
Kevin Rutherford <kevin@rutherford-software.com>
2024-06-26 19:55:26 +0100

    Implement a characterisation test #115

--- test/restricted-domain/client-can-see-activity.test.ts
+++ test/restricted-domain/client-can-see-activity.test.ts
@@ -1,131 +1,160 @@
+import { Domain } from '../../src/domain/index.open'
 import * as RestrictedDomain from '../../src/restricted-domain'
 import * as UnrestrictedDomain from '../../src/unrestricted-domain'
 import { defaultTestObserver } from '../default-test-observer'
 import { arbitraryString, arbitraryWord } from '../helpers'
 import { mkEvent } from '../mk-event'
 
-describe('given a client with no privileges', () => {
-  const claims = () => false
-  const unrestrictedDomain = UnrestrictedDomain.instantiate(defaultTestObserver)
-  const restrictedQueries = RestrictedDomain.instantiate(claims, unrestrictedDomain.queries)
-  const handleEvent = unrestrictedDomain.handleEvent
+describe('client-can-see-activity', () => {
+  let handleEvent: UnrestrictedDomain.EventHandler
+  let unrestrictedQueries: Domain
+  let restrictedQueries: Domain
 
-  describe('when community-created', () => {
-    it.todo('the activity is visible')
+  beforeEach(() => {
+    const unrestrictedDomain = UnrestrictedDomain.instantiate(defaultTestObserver)
+    handleEvent = unrestrictedDomain.handleEvent
+    unrestrictedQueries = unrestrictedDomain.queries
   })
 
-  describe('when collection-created (public)', () => {
-    it.todo('the activity is visible')
-  })
-
-  describe('when collection-created (private)', () => {
-    it.todo('the activity is not visible')
-  })
-
-  describe('when doi-entered (in a public collection)', () => {
-    const collectionId = arbitraryWord()
+  describe('given a client with no privileges', () => {
+    const claims = () => false
 
     beforeEach(() => {
-      handleEvent(mkEvent('collection-created', {
-        id: collectionId,
-        name: arbitraryString(),
-        description: arbitraryString(),
-      }))
-      handleEvent(mkEvent('doi-entered', {
-        id: arbitraryWord(),
-        workId: arbitraryWord(),
-        collectionId,
-      }))
+      restrictedQueries = RestrictedDomain.instantiate(claims, unrestrictedQueries)
     })
 
-    it('the activity is visible', () => {
-      const activities = restrictedQueries.getLocalTimeline()
-      expect(activities).toHaveLength(2)
+    describe('when community-created', () => {
+      it.todo('the activity is visible')
+    })
+
+    describe('when collection-created (public)', () => {
+      const collectionId = arbitraryWord()
+
+      beforeEach(() => {
+        handleEvent(mkEvent('collection-created', {
+          id: collectionId,
+          name: arbitraryString(),
+          description: arbitraryString(),
+        }))
+      })
+
+      it('the activity is visible', () => {
+        const activities = restrictedQueries.getLocalTimeline()
+        expect(activities).toHaveLength(1)
+        // SMELL: check details of the activity
+      })
+    })
+
+    describe('when collection-created (private)', () => {
+      it.todo('the activity is not visible')
+    })
+
+    describe('when doi-entered (in a public collection)', () => {
+      const collectionId = arbitraryWord()
+
+      beforeEach(() => {
+        handleEvent(mkEvent('collection-created', {
+          id: collectionId,
+          name: arbitraryString(),
+          description: arbitraryString(),
+        }))
+        handleEvent(mkEvent('doi-entered', {
+          id: arbitraryWord(),
+          workId: arbitraryWord(),
+          collectionId,
+        }))
+      })
+
+      it('the activity is visible', () => {
+        const activities = restrictedQueries.getLocalTimeline()
+        expect(activities).toHaveLength(2)
+        // SMELL: check details of the second activity
+      })
+    })
+
+    describe('when doi-entered (in a private collection)', () => {
+      it.todo('the activity is not visible')
+    })
+
+    describe('when comment-created (in a public collection)', () => {
+      it.todo('the activity is visible')
+    })
+
+    describe('when comment-created (in a private collection)', () => {
+      it.todo('the activity is not visible')
+    })
+
+    describe('when work-updated', () => {
+      it.todo('the activity is not visible')
     })
   })
 
-  describe('when doi-entered (in a private collection)', () => {
-    it.todo('the activity is not visible')
+  describe('given a client with browse-private-collections privilege', () => {
+    describe('when community-created', () => {
+      it.todo('the activity is visible')
+    })
+
+    describe('when collection-created (public)', () => {
+      it.todo('the activity is visible')
+    })
+
+    describe('when collection-created (private)', () => {
+      it.todo('the activity is visible')
+    })
+
+    describe('when doi-entered (in a public collection)', () => {
+      it.todo('the activity is visible')
+    })
+
+    describe('when doi-entered (in a private collection)', () => {
+      it.todo('the activity is visible')
+    })
+
+    describe('when comment-created (in a public collection)', () => {
+      it.todo('the activity is visible')
+    })
+
+    describe('when comment-created (in a private collection)', () => {
+      it.todo('the activity is visible')
+    })
+
+    describe('when work-updated', () => {
+      it.todo('the activity is not visible')
+    })
   })
 
-  describe('when comment-created (in a public collection)', () => {
-    it.todo('the activity is visible')
-  })
+  describe('given a client with receive-work-updates privilege', () => {
+    describe('when community-created', () => {
+      it.todo('the activity is visible')
+    })
 
-  describe('when comment-created (in a private collection)', () => {
-    it.todo('the activity is not visible')
-  })
+    describe('when collection-created (public)', () => {
+      it.todo('the activity is visible')
+    })
 
-  describe('when work-updated', () => {
-    it.todo('the activity is not visible')
-  })
-})
+    describe('when collection-created (private)', () => {
+      it.todo('the activity is not visible')
+    })
 
-describe('given a client with browse-private-collections privilege', () => {
-  describe('when community-created', () => {
-    it.todo('the activity is visible')
-  })
+    describe('when doi-entered (in a public collection)', () => {
+      it.todo('the activity is visible')
+    })
 
-  describe('when collection-created (public)', () => {
-    it.todo('the activity is visible')
-  })
+    describe('when doi-entered (in a private collection)', () => {
+      it.todo('the activity is not visible')
+    })
 
-  describe('when collection-created (private)', () => {
-    it.todo('the activity is visible')
-  })
+    describe('when comment-created (in a public collection)', () => {
+      it.todo('the activity is visible')
+    })
 
-  describe('when doi-entered (in a public collection)', () => {
-    it.todo('the activity is visible')
-  })
+    describe('when comment-created (in a private collection)', () => {
+      it.todo('the activity is not visible')
+    })
 
-  describe('when doi-entered (in a private collection)', () => {
-    it.todo('the activity is visible')
-  })
-
-  describe('when comment-created (in a public collection)', () => {
-    it.todo('the activity is visible')
-  })
-
-  describe('when comment-created (in a private collection)', () => {
-    it.todo('the activity is visible')
-  })
-
-  describe('when work-updated', () => {
-    it.todo('the activity is not visible')
-  })
-})
-
-describe('given a client with receive-work-updates privilege', () => {
-  describe('when community-created', () => {
-    it.todo('the activity is visible')
-  })
-
-  describe('when collection-created (public)', () => {
-    it.todo('the activity is visible')
-  })
-
-  describe('when collection-created (private)', () => {
-    it.todo('the activity is not visible')
-  })
-
-  describe('when doi-entered (in a public collection)', () => {
-    it.todo('the activity is visible')
-  })
-
-  describe('when doi-entered (in a private collection)', () => {
-    it.todo('the activity is not visible')
-  })
-
-  describe('when comment-created (in a public collection)', () => {
-    it.todo('the activity is visible')
-  })
-
-  describe('when comment-created (in a private collection)', () => {
-    it.todo('the activity is not visible')
-  })
-
-  describe('when work-updated', () => {
-    it.todo('the activity is visible')
+    describe('when work-updated', () => {
+      it.todo('the activity is visible')
+    })
   })
 })
 
