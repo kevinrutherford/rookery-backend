b4a13c6eabc69419b1e75f75fc930c6420703cd1
Kevin Rutherford <kevin@rutherford-software.com>
2024-06-30 16:50:14 +0100

    Remove more test setup duplication #115

--- test/restricted-domain/client-can-see-activity.test.ts
+++ test/restricted-domain/client-can-see-activity.test.ts
@@ -65,6 +65,7 @@ type Examples = ReadonlyArray<Example>
 
 const noPrivileges: Authority = () => false
 const canBrowsePrivateCollections:Authority = (requiredScope) => requiredScope === 'browse-private-collections'
+const canReceiveWorkUpdates:Authority = (requiredScope) => requiredScope === 'receive-work-updates'
 
 describe('client-can-see-activity', () => {
 
@@ -89,6 +90,16 @@ describe('client-can-see-activity', () => {
       // [privateCollectionWithEntry, addComment, 1],
       // [emptyDatabase, updateWork, 0],
     ] satisfies Examples],
+    [canReceiveWorkUpdates, [
+      // [emptyDatabase, createCommunity, 1],
+      [emptyDatabase, createCollection, 1],
+      // [emptyDatabase, createPrivateCollection, 0],
+      [emptyCollection, addEntry, 1],
+      [emptyPrivateCollection, addEntry, 0],
+      [publicCollectionWithEntry, addComment, 1],
+      [privateCollectionWithEntry, addComment, 0],
+      // [emptyDatabase, updateWork, 1],
+    ] satisfies Examples],
   ])('client-can-see-activity', (claims: Authority, examples: Examples) => {
     let handleEvent: UnrestrictedDomain.EventHandler
     let unrestrictedQueries: Domain
@@ -105,40 +116,6 @@ describe('client-can-see-activity', () => {
       const initialActivityCount = restrictedQueries.getLocalTimeline().length
       event(initialState)
       expect(restrictedQueries.getLocalTimeline().length - initialActivityCount).toBe(activitiesAdded)
-    })
-  })
-
-  describe('given a client with receive-work-updates privilege', () => {
-    describe('when community-created', () => {
-      it.todo('the activity is visible')
-    })
-
-    describe('when collection-created (public)', () => {
-      it.todo('the activity is visible')
-    })
-
-    describe('when collection-created (private)', () => {
-      it.todo('the activity is not visible')
-    })
-
-    describe('when doi-entered (in a public collection)', () => {
-      it.todo('the activity is visible')
-    })
-
-    describe('when doi-entered (in a private collection)', () => {
-      it.todo('the activity is not visible')
-    })
-
-    describe('when comment-created (in a public collection)', () => {
-      it.todo('the activity is visible')
-    })
-
-    describe('when comment-created (in a private collection)', () => {
-      it.todo('the activity is not visible')
-    })
-
-    describe('when work-updated', () => {
-      it.todo('the activity is visible')
     })
   })
 })
