61a3e464fc89da16127996023ae48820550aff91
Kevin Rutherford <kevin@rutherford-software.com>
2024-07-04 07:50:16 +0100

    Remove a duplicate type #129

--- src/unrestricted-domain/state/timeline-event.ts
+++ src/unrestricted-domain/state/timeline-event.ts
@@ -1,19 +1,6 @@
-import { Work } from './work'
 import {
-  CollectionCreated, CommentCreated, CommunityCreated, DoiEntered,
+  CollectionCreated, CommentCreated, CommunityCreated, DoiEntered, WorkUpdated,
 } from '../../domain/domain'
-
-type WorkUpdated = {
-  type: 'work-updated',
-  id: string, // SMELL -- duplicated domain object types
-  created: Date,
-  actor: string,
-  occurredWithinPrivateCollection: boolean,
-  data: {
-    workId: string,
-    attributes: Work['frontMatter'],
-  },
-}
 
 export type TimelineEvent =
   | CommunityCreated
