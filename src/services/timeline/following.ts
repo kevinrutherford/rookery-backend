0ed22cb5b93575c54f20354e6fdbb2f830af79ec
Kevin Rutherford <kevin@rutherford-software.com>
2024-07-17 19:28:20 +0100

    Add a hardcoded update to the following feed

--- src/services/timeline/following.ts
+++ src/services/timeline/following.ts
@@ -1,8 +1,30 @@
 import * as E from 'fp-ts/Either'
+import { renderAccountIdentifier } from '../json-api/render-account-identifier'
 import { Service } from '../service'
 
 export const getFollowingTimeline = (): Service => () => E.right({
-  data: [],
-  included: [],
+  data: [
+    {
+      type: 'update:comment-created',
+      id: '123',
+      attributes: {
+        content: 'We definitely need to reference this paper',
+        occurred_at: new Date().toISOString(),
+      },
+      relationships: {
+        actor: { data: renderAccountIdentifier('@voldemort@rookery-1.xpsurgery.com') },
+      },
+    },
+  ],
+  included: [
+    {
+      ...renderAccountIdentifier('@voldemort@rookery-1.xpsurgery.com'),
+      attributes: {
+        username: '@voldemort@rookery-1.xpsurgery.com',
+        display_name: 'Voldemort',
+        avatar_url: 'https://upload.wikimedia.org/wikipedia/en/a/a3/Lordvoldemort.jpg',
+      },
+    },
+  ],
 })
 
