b4553b6fd02f5723a77e2625923ae6251d78a455
Kevin Rutherford <kevin@rutherford-software.com>
2024-07-22 12:31:36 +0100

    Use standard renderers for hardcoded timeline updates to reduce coupling

--- src/services/timeline/federated.ts
+++ src/services/timeline/federated.ts
@@ -1,6 +1,36 @@
 import * as E from 'fp-ts/Either'
+import { renderAccount } from '../json-api/render-account'
 import { renderAccountIdentifier } from '../json-api/render-account-identifier'
+import { renderEntry } from '../json-api/render-entry'
+import { renderEntryIdentifier } from '../json-api/render-entry-identifier'
+import { renderWork } from '../json-api/render-work'
+import { renderWorkIdentifier } from '../json-api/render-work-identifier'
 import { Service } from '../service'
+
+const account = {
+  id: '@voldemort@rookery-1.xpsurgery.com',
+  username: '@voldemort@rookery-1.xpsurgery.com',
+  displayName: 'Voldemort',
+  avatarUrl: 'https://upload.wikimedia.org/wikipedia/en/a/a3/Lordvoldemort.jpg',
+}
+
+const work = {
+  id: '5678',
+  doi: '10.1234/45678',
+  updatedAt: new Date(),
+  frontMatter: {
+    crossrefStatus: 'not-determined' as const,
+    reason: 'never-fetched' as const,
+  },
+}
+
+const entry = {
+  id: '1234',
+  workId: work.id,
+  collectionId: 'snape_stuff',
+  commentsCount: 13,
+  addedAt: new Date(),
+}
 
 export const getFederatedTimeline = (): Service => () => E.right({
   data: [
@@ -8,23 +38,19 @@ export const getFederatedTimeline = (): Service => () => E.right({
       type: 'update:comment-created',
       id: '123',
       attributes: {
-        content: 'We definitely need to reference this paper',
         occurred_at: new Date().toISOString(),
       },
       relationships: {
-        actor: { data: renderAccountIdentifier('@voldemort@rookery-1.xpsurgery.com') },
+        actor: { data: renderAccountIdentifier(account.id) },
+        entry: { data: renderEntryIdentifier(entry.id) },
+        work: { data: renderWorkIdentifier(work.id) },
       },
     },
   ],
   included: [
-    {
-      ...renderAccountIdentifier('@voldemort@rookery-1.xpsurgery.com'),
-      attributes: {
-        username: '@voldemort@rookery-1.xpsurgery.com',
-        display_name: 'Voldemort',
-        avatar_url: 'https://upload.wikimedia.org/wikipedia/en/a/a3/Lordvoldemort.jpg',
-      },
-    },
+    renderAccount(account),
+    renderEntry(entry),
+    renderWork(work),
   ],
 })
 
