ab93ff39f189f9018f8449d2ad2e50d3c98481fb
Kevin Rutherford <kevin@rutherford-software.com>
2024-07-28 16:33:48 +0100

    Accept a wider set of auth tokens for now

--- src/auth/authority.ts
+++ src/auth/authority.ts
@@ -2,6 +2,6 @@ export type Authority = (requiredScope: string) => boolean
 
 export const instantiate = (context: string | undefined): Authority => () => (
   context !== undefined
-  && context === process.env.DEVELOPMENT_BEARER_TOKEN
+  && context !== ''
 )
 
