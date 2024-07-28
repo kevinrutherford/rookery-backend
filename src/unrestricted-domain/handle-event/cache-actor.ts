f197f881c28fa86b65e1b2b7f7fdb8021cd7887c
Kevin Rutherford <kevin@rutherford-software.com>
2024-07-28 14:54:25 +0100

    Populate the members readmodel from environment variables

--- src/unrestricted-domain/handle-event/cache-actor.ts
+++ src/unrestricted-domain/handle-event/cache-actor.ts
@@ -2,5 +2,37 @@ import { Readmodel } from '../state/readmodel'
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const cacheActor = (state: Readmodel, actorId: string): void => {
+  if (actorId === process.env.USER_1_ID) {
+    state.members.set(actorId, {
+      id: actorId,
+      username: process.env.USER_1_USERNAME ?? 'Missing env var',
+      displayName: process.env.USER_1_DISPLAYNAME ?? 'Missing env var',
+      avatarUrl: process.env.USER_1_AVATAR_URL ?? 'Missing env var',
+    })
+  }
+  if (actorId === process.env.USER_2_ID) {
+    state.members.set(actorId, {
+      id: actorId,
+      username: process.env.USER_2_USERNAME ?? 'Missing env var',
+      displayName: process.env.USER_2_DISPLAYNAME ?? 'Missing env var',
+      avatarUrl: process.env.USER_2_AVATAR_URL ?? 'Missing env var',
+    })
+  }
+  if (actorId === process.env.USER_3_ID) {
+    state.members.set(actorId, {
+      id: actorId,
+      username: process.env.USER_3_USERNAME ?? 'Missing env var',
+      displayName: process.env.USER_3_DISPLAYNAME ?? 'Missing env var',
+      avatarUrl: process.env.USER_3_AVATAR_URL ?? 'Missing env var',
+    })
+  }
+  if (actorId === process.env.USER_CRB_ID) {
+    state.members.set(actorId, {
+      id: actorId,
+      username: process.env.USER_CRB_USERNAME ?? 'Missing env var',
+      displayName: process.env.USER_CRB_DISPLAYNAME ?? 'Missing env var',
+      avatarUrl: process.env.USER_CRB_AVATAR_URL ?? 'Missing env var',
+    })
+  }
 }
 
