722b4c16178858b74ca96c78112414e0628e47f3
Kevin Rutherford <kevin@rutherford-software.com>
2024-08-08 17:09:54 +0100

    Push hardcoded comment update down into hte readmodel

--- src/services/timeline/followed.ts
+++ src/services/timeline/followed.ts
@@ -1,58 +1,12 @@
 import * as E from 'fp-ts/Either'
-import { renderEntry } from '../json-api/render-entry'
-import { renderEntryIdentifier } from '../json-api/render-entry-identifier'
-import { renderMember } from '../json-api/render-member'
-import { renderMemberIdentifier } from '../json-api/render-member-identifier'
-import { renderWork } from '../json-api/render-work'
-import { renderWorkIdentifier } from '../json-api/render-work-identifier'
+import { pipe } from 'fp-ts/function'
+import { renderAsFeed } from './render-as-feed'
+import { Domain } from '../../domain/domain'
 import { Service } from '../service'
 
-const account = {
-  id: '@voldemort@rookery-1.xpsurgery.com',
-  username: '@voldemort@rookery-1.xpsurgery.com',
-  displayName: 'Voldemort',
-  avatarUrl: 'https://upload.wikimedia.org/wikipedia/en/a/a3/Lordvoldemort.jpg',
-  following: [],
-  cache: 'fetching' as const,
-}
-
-const work = {
-  id: '5678',
-  doi: '10.1234/45678',
-  updatedAt: new Date(),
-  frontMatter: {
-    crossrefStatus: 'not-determined' as const,
-    reason: 'never-fetched' as const,
-  },
-}
-
-const entry = {
-  id: '1234',
-  workId: work.id,
-  collectionId: 'snape_stuff',
-  commentsCount: 13,
-  addedAt: new Date(),
-}
-
-export const getFollowedTimeline = (): Service => () => E.right({
-  data: [
-    {
-      type: 'update:comment-created',
-      id: '123',
-      attributes: {
-        occurred_at: new Date().toISOString(),
-      },
-      relationships: {
-        actor: { data: renderMemberIdentifier(account.id) },
-        entry: { data: renderEntryIdentifier(entry.id) },
-        work: { data: renderWorkIdentifier(work.id) },
-      },
-    },
-  ],
-  included: [
-    renderMember(account),
-    renderEntry(entry),
-    renderWork(work),
-  ],
-})
+export const getFollowedTimeline = (queries: Domain): Service => () => pipe(
+  queries.getFollowedTimeline(),
+  renderAsFeed(queries),
+  E.right,
+)
 
