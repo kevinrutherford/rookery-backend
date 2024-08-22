ee78191012e6bf0e0311471f1f6425ab35dca544
Kevin Rutherford <kevin@rutherford-software.com>
2024-08-22 16:24:18 +0100

    Introduce a seam to allow the discussion to eventually be omitted from the update

--- src/services/timeline/render-inbox-comment-created.ts
+++ src/services/timeline/render-inbox-comment-created.ts
@@ -1,4 +1,6 @@
+import * as E from 'fp-ts/Either'
 import * as O from 'fp-ts/Option'
+import { pipe } from 'fp-ts/function'
 import { includeMember } from './include-member'
 import { UpdateWithIncludes } from './update-with-includes'
 import { Domain } from '../../domain/index.open'
@@ -7,21 +9,44 @@ import { renderDiscussion } from '../json-api/render-discussion'
 import { renderDiscussionIdentifier } from '../json-api/render-discussion-identifier'
 import { renderMemberIdentifier } from '../json-api/render-member-identifier'
 
-export const renderInboxCommentCreated = (queries: Domain, update: InboxCommentCreated): UpdateWithIncludes => ({
-  data: {
-    type: update.kind,
-    id: update.id,
-    attributes: {
-      occurred_at: update.created.toISOString(),
-    },
-    relationships: {
-      actor: { data: renderMemberIdentifier(update.actorId) },
-      entry: { data: renderDiscussionIdentifier(update.discussion.id) },
-    },
-  },
-  included: [
-    includeMember(queries, update.actorId),
-    O.some(renderDiscussion(update.discussion)),
-  ],
-})
+export const renderInboxCommentCreated = (queries: Domain, update: InboxCommentCreated): UpdateWithIncludes => pipe(
+  update.discussionId,
+  queries.lookupEntry,
+  E.match(
+    () => ({
+      data: {
+        type: update.kind,
+        id: update.id,
+        attributes: {
+          occurred_at: update.created.toISOString(),
+        },
+        relationships: {
+          actor: { data: renderMemberIdentifier(update.actorId) },
+          entry: { data: renderDiscussionIdentifier(update.discussionId) },
+        },
+      },
+      included: [
+        includeMember(queries, update.actorId),
+        O.some(renderDiscussion(update.discussion)),
+      ],
+    }),
+    (discussion) => ({
+      data: {
+        type: update.kind,
+        id: update.id,
+        attributes: {
+          occurred_at: update.created.toISOString(),
+        },
+        relationships: {
+          actor: { data: renderMemberIdentifier(update.actorId) },
+          entry: { data: renderDiscussionIdentifier(update.discussionId) },
+        },
+      },
+      included: [
+        includeMember(queries, update.actorId),
+        O.some(renderDiscussion(discussion)),
+      ],
+    }),
+  ),
+)
 
