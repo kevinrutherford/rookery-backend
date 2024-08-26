2d6c7fd23d91904bf75c6b7be41c8b2328d44d2c
Kevin Rutherford <kevin@rutherford-software.com>
2024-08-26 12:09:22 +0100

    Only return not-found if the local member does not exist

--- src/unrestricted-domain/queries/get-followers.ts
+++ src/unrestricted-domain/queries/get-followers.ts
@@ -1,4 +1,5 @@
 import * as E from 'fp-ts/Either'
+import * as O from 'fp-ts/Option'
 import * as RM from 'fp-ts/ReadonlyMap'
 import { pipe } from 'fp-ts/function'
 import * as S from 'fp-ts/string'
@@ -6,8 +7,13 @@ import { Domain } from '../../domain/index.open'
 import { Readmodel } from '../state/readmodel'
 
 export const getFollowers = (currentState: Readmodel): Domain['getFollowers'] => (memberId) => pipe(
-  currentState.followers,
+  currentState.members,
   RM.lookup(S.Eq)(memberId),
-  E.fromOption(() => 'not-found'),
+  E.fromOption(() => 'not-found' as const),
+  E.map(() => pipe(
+    currentState.followers,
+    RM.lookup(S.Eq)(memberId),
+    O.getOrElse(() => [] as ReadonlyArray<string>),
+  )),
 )
 
