50f86da5f61ac83679f042d4e26eb73fc5ca4ec1
Kevin Rutherford <kevin@rutherford-software.com>
2024-08-28 08:42:37 +0100

    Cope with eventstore details in event debug messages

--- src/app/report-events.ts
+++ src/app/report-events.ts
@@ -1,3 +1,5 @@
+import * as RA from 'fp-ts/ReadonlyArray'
+import { pipe } from 'fp-ts/function'
 import { Domain } from '../domain/index.open'
 import * as Logger from '../logger'
 import { DomainObserver } from '../unrestricted-domain/index.open'
@@ -5,11 +7,20 @@ import { DomainObserver } from '../unrestricted-domain/index.open'
 export const reportEvents = (logger: Logger.Logger): DomainObserver => (domain: Domain) => {
   const info = domain.info()
   logger.info('Events handled', { count: info.eventsCount })
-  if (info.unexpectedEvents.length > 0)
-    logger.warn('Unexpected events received', { events: info.unexpectedEvents })
+  if (info.unexpectedEvents.length > 0) {
+    logger.warn('Unexpected events received', {
+      events: pipe(
+        info.unexpectedEvents, // SMELL -- unnecessary eventstore details
+        RA.map((event) => JSON.stringify(event, (_, v) => (typeof v === 'bigint' ? v.toString() : v))),
+      ),
+    })
+  }
   if (info.unrecognisedEvents.length > 0) {
     logger.error('Unrecognised event; terminating', {
-      events: info.unrecognisedEvents,
+      events: pipe(
+        info.unrecognisedEvents, // SMELL -- unnecessary eventstore details
+        RA.map((event) => JSON.stringify(event, (_, v) => (typeof v === 'bigint' ? v.toString() : v))),
+      ),
     })
     process.exit(1)
   }
--- src/services/root/root.ts
+++ src/services/root/root.ts
@@ -1,5 +1,6 @@
 import * as E from 'fp-ts/Either'
 import * as O from 'fp-ts/Option'
+import * as RA from 'fp-ts/ReadonlyArray'
 import { pipe } from 'fp-ts/function'
 import { Domain } from '../../domain/index.open'
 import { renderCommunityIdentifier } from '../json-api/render-community-identifier'
@@ -26,7 +27,20 @@ export const getRoot = (queries: Domain): Service => () => pipe(
           links: { related: pathToCommunity() },
         },
       },
-      meta: queries.info(),
+      meta: pipe(
+        queries.info(),
+        (probe) => ({
+          eventsCount: probe.eventsCount,
+          unexpectedEvents: pipe(
+            probe.unexpectedEvents, // SMELL -- unnecessary eventstore details
+            RA.map((event) => JSON.stringify(event, (_, v) => (typeof v === 'bigint' ? v.toString() : v))),
+          ),
+          unrecognisedEvents: pipe(
+            probe.unrecognisedEvents, // SMELL -- unnecessary eventstore details
+            RA.map((event) => JSON.stringify(event, (_, v) => (typeof v === 'bigint' ? v.toString() : v))),
+          ),
+        }),
+      ),
     },
   },
   E.right,
